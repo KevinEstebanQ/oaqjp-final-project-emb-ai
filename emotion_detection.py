4d1eebf72ea606dd130d39cbfaf0a7390e216341
KevinEstebanQ <kevinestebanquicenocardenas@gmail.com>
2026-02-18 16:11:07 -0500

    initialized the first version of project

---
+++ emotion_detection.py
@@ -0,0 +1,28 @@
+import requests
+
+def emotion_detector(text_to_analyse):
+    """
+    Docstring for emotion_detector
+    
+    :param text_to_analyse: call the watson ai NLP and generate a sentiment analysis
+
+    """
+
+    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = { "raw_document": { "text": text_to_analyse } }
+    response = requests.post(url=URL, json=input_json, headers=header)
+     
+    return response.text
+
+import requests  # Import the requests library to handle HTTP requests
+def sentiment_analyzer(text_to_analyse):  # Define a function named sentiment_analyzer that takes a string input (text_to_analyse)
+    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'  # URL of the sentiment analysis service
+    myobj = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analyzed
+    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}  # Set the headers required for the API request
+    response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
+    return response.text  # Return the response text from the API
+
+ans = sentiment_analyzer("hello im happy")
+print(ans)
+
